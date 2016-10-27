975bd7984e7832909fe96baa5ba9a9ec7a1e1946
Tyler <tgmilka@gmail.com>
2016-10-26 22:14:42 -0500

    css and html all finished

---
+++ week-3-game/assets/javascript/game.js
@@ -0,0 +1,11 @@
+function getRandom(){
+
+	console.log();
+	var ranLetter= Math.floor(Math.random()*27);
+	return ranLetter;
+}
+
+function getLetter(){
+	
+	var letterBank = ["abcdefghijklmnopqrstuvwxyz"].split('');
+}
